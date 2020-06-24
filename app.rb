f1292aad0f2db3d6692e69e714dc08391b5c2e35
ark <arkadii@darly.solutions>
2020-06-24 16:48:34 +0300

    rm comments

--- app.rb
+++ app.rb
@@ -1,21 +1,8 @@
 require 'roda'
 
-# @resource Account ownership
-#
-# This document describes the API for creating, reading, and deleting account ownerships.
-#
 class App < Roda
   plugin :hash_routes
-  ##
-  # Returns a list of ownerships associated with the account.
-  #
-  # Status can be -1(Deleted), 0(Inactive), 1(Active), 2(Expired) and 3(Cancelled).
-  #
-  # @path [GET] /default
-  #
-  # @parameter offset   [integer]               Used for pagination of response data (default: 25 items per response). Specifies the offset of the next block of data to receive.
-  # @parameter status   [array<string>]         Filter by status. (e.g. status[]=1&status[]=2&status[]=3).
-  # @parameter sort_order [enum<id,begin_at,end_at,created_at>]  Orders response by fields. (e.g. sort_order=created_at).
+
   hash_routes "" do
     #
     # @path [GET] /a
